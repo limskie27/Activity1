7ba3c8a0ddd76aabfa37b3163df59b0b09f58a3f
Limmuel Espiritu <espiritu.limmuel@colm.edu.ph>
2025-01-30 13:22:22 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Overload x = new Overload();
+
+            double triangle = x.CalculateArea(10, 15, 0.5);
+            listBox1.Items.Add("Triangle Area: " + triangle);
+            textBox1.Text = triangle.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Overload x = new Overload();
+
+            int rectangle = x.CalculateArea(25, 50);
+            listBox1.Items.Add("Rectangle Area: " + rectangle);
+            textBox1.Text = rectangle.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Overload x = new Overload();
+
+            double circle = x.CalculateArea(20);
+            listBox1.Items.Add("Circle Area: " + circle);
+            textBox1.Text = circle.ToString();
+        }
+    }
+}
---
+++ Overload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Overload
+    {
+        public int length;
+        public int width;
+        public int bases;
+        public int height;
+        public int area;
+        public double triangle = 0.5;
+
+        public int CalculateArea(int width, int length)
+        {
+            return width * length;
+        }
+
+        public double CalculateArea(double length, double height, double triangle)
+        {
+            return triangle * length * height;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
